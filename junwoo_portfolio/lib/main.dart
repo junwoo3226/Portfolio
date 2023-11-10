078648896637ddd1b58b160d8abdcebca011fa7d
DESKTOP-9IE11B3\Dev02 <wnsdn3226@nate.com>
2023-11-10 10:05:37 +0900

    1. 앱바 아이콘 변경 앱바 아이콘은 index.html에서 변경해야함

--- junwoo_portfolio/lib/main.dart
+++ junwoo_portfolio/lib/main.dart
@@ -12,23 +12,46 @@ class JunWooMain extends StatelessWidget {
     return MaterialApp(
         // 디버그 표시를 없앤다.
         debugShowCheckedModeBanner: false,
-        title: 'Junwoo Portfolio',
+        title: '황준우',
         theme: ThemeData(
           primarySwatch: Colors.deepPurple,
         ),
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              '황준우',
+            appBar: AppBar(
+              title: const Text(
+                '황준우',
+              ),
+              centerTitle: true,
             ),
-            centerTitle: true,
-          ),
-          body: Center(
-              child: Image.asset(
-            "image/junwoo.jpg",
-            width: 500,
-            height: 500,
-          )),
-        ));
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(0, 50.0, 0, 0),
+                    padding: const EdgeInsets.all(20.0),
+                    width: 300,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      // shade100은 상수타입으로 될수가 없음
+                      // 그이유는 shade는 객체이기 때문임
+                      color: Colors.deepPurple.shade300,
+                    ),
+                    child: ClipOval(
+                      child: Center(
+                        child: Image.asset(
+                          'image/junwoo.jpg',
+                          width: 280,
+                          height: 280,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )));
   }
 }
